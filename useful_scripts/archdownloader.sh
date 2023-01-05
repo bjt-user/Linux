bb9e1e7bf3b3881bf4ad136532e0c01c55a09344
BJT User <bjoern.foersterling@web.de>
2023-01-05 20:37:21 +0000

    Update archdownloader.sh

--- useful_scripts/archdownloader.sh
+++ useful_scripts/archdownloader.sh
@@ -4,4 +4,9 @@ wget http://ftp.tu-chemnitz.de/pub/linux/archlinux/iso/latest/archlinux-x86_64.i
 wget http://ftp.tu-chemnitz.de/pub/linux/archlinux/iso/latest/archlinux-x86_64.iso.sig -O ~/Downloads/archlinux-x86_64.iso.sig
 gpg --keyserver-options auto-key-retrieve --verify ~/Downloads/archlinux-x86_64.iso.sig
 
+# add this line to get the name of the iso and make sure it is the right file type
+# this is how the output should look like:
+# ISO 9660 CD-ROM filesystem data (DOS/MBR boot sector) 'ARCH_202301' (bootable)
+file ~/Downloads/archlinux-x86_64.iso
+
 exit 0
