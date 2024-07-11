5e8e03cbb171508e1b61cccfb7996512f70d3e01
bjt-user <bjoern.foersterling@web.de>
2024-07-11 19:04:21 +0200

    deleted old, barely working script

--- htmlparsing/script.sh
+++
@@ -1,4 +0,0 @@
-wget -U mozilla -O wetter.txt 'https://www.br.de/wetter/action/5_tage_prognose.do?id=0&plz=10646&regio=Unterfranken&x=9&y=10'
-sed -i 's/<//g' wetter.txt
-sed -i 's/>//g' wetter.txt
-cat wetter.txt
